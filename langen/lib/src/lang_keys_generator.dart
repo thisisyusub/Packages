04392a7516837ceaf8af5055acd5c57259105d7d
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-09-21 12:16:36 +0400

    changed

--- langen/lib/src/lang_keys_generator.dart
+++ langen/lib/src/lang_keys_generator.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:build/build.dart';
 
@@ -8,9 +9,28 @@ class LangKeysGenerator implements Builder {
         '.json': ['lang_keys.dart'],
       };
 
+  /// used to store keys of language keys for documentation
+  /// will be used to control if the current file contains
+  /// current json key or not
+  final languageKeysWithJsonKeys = <String, HashSet<String>>{};
+
+  /// used to store values of keys for documentation
+  final languageKeysWithJsonValues = <String, Map<String, dynamic>>{};
+
+  /// stores [all keys] used in app
+  final valueKeys = HashSet<String>();
+
+  /// stores all languageCodes used in app
+  final languageCodes = HashSet<String>();
+
   @override
   FutureOr<void> build(BuildStep buildStep) {
     final assetId = buildStep.inputId;
-    print(assetId.path);
+
+    if (assetId.pathSegments.contains('langs')) {
+      print(assetId.package);
+      print(assetId.path);
+      print(assetId.pathSegments);
+    }
   }
 }
