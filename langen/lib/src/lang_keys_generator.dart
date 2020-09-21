0aa16cdd591bd6ff0c84df434e8f16fc5897352d
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-09-21 12:48:51 +0400

    changed

--- langen/lib/src/lang_keys_generator.dart
+++ langen/lib/src/lang_keys_generator.dart
@@ -4,6 +4,17 @@ import 'dart:convert' show json;
 
 import 'package:build/build.dart';
 
+extension on String {
+  String get capitalize {
+    final splitParts = split('_');
+    var res = splitParts[0];
+    for (var i = 1; i < splitParts.length; i++) {
+      res += splitParts[i][0].toUpperCase() + splitParts[i].substring(1);
+    }
+    return res;
+  }
+}
+
 class LangKeysGenerator implements Builder {
   @override
   Map<String, List<String>> get buildExtensions => {
@@ -17,6 +28,9 @@ class LangKeysGenerator implements Builder {
 
   /// used to store values of keys for documentation
   final languageKeysWithJsonValues = <String, Map<String, dynamic>>{};
+
+  /// stores [all keys] used in app
+  final valueKeys = HashSet<String>();
 
   /// stores all languageCodes used in app
   final languageCodes = HashSet<String>();
@@ -33,12 +47,56 @@ class LangKeysGenerator implements Builder {
       final file = await buildStep.readAsString(assetId);
 
       /// converts json string to Map
-      Map<String, dynamic> keyAndValues = json.decode(file);
+      Map<String, dynamic> keysAndValues = json.decode(file);
 
       /// adds [language key] to HashSet
       languageCodes.add(langCode);
 
-      print(languageCodes);
+      /// collects all keys of current language
+      final currentLangKeys = HashSet<String>();
+
+      final currentLangWithValues = <String, dynamic>{};
+
+      /// collects all values of current langauge
+      for (var keyAndValue in keysAndValues.entries) {
+        valueKeys.add(keyAndValue.key);
+        currentLangKeys.add(keyAndValue.key);
+        currentLangWithValues.putIfAbsent(
+            keyAndValue.key, () => keyAndValue.value);
+      }
+
+      languageKeysWithJsonKeys.putIfAbsent(langCode, () => currentLangKeys);
+      languageKeysWithJsonValues.putIfAbsent(
+          langCode, () => currentLangWithValues);
+
+      /// to perform fast String concatenation
+      final sb = StringBuffer();
+
+      for (var i = 0; i < valueKeys.length; i++) {
+        final currentKey = valueKeys.elementAt(i);
+
+        for (var langCode in languageCodes) {
+          final isCurrentLangFileContainsCurentKey =
+              languageKeysWithJsonKeys[langCode].contains(currentKey);
+
+          final message = isCurrentLangFileContainsCurentKey
+              ? languageKeysWithJsonValues[langCode][currentKey]
+              : '[Null]';
+
+          sb.writeln(
+            '  /// ${isCurrentLangFileContainsCurentKey ? '' : '!'}'
+            ' ${langCode.toUpperCase()}: ${message.replaceAll('\n', ' ')}',
+          );
+        }
+        sb.writeln(' const String ${currentKey.capitalize} = \'$currentKey\';');
+
+        if (i != valueKeys.length - 1) {
+          sb.writeln();
+        }
+      }
+
+      final copyAssetId = assetId.changeExtension('.g.dart');
+      await buildStep.writeAsString(copyAssetId, sb.toString());
     }
   }
 }
